7b7dfdf6bb262f1647b4d3af60d5800731500883
Jader Feijo <jader.feijo@gmail.com>
2024-05-08 17:25:18 +0100

    Implement basic input/output types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-export function sayHello(name: string): string {
-	return `Hello, ${name}`;
+import { Input, Output } from './types';
+export * from './types';
+
+export async function execute(inputs: [Input<any>]): Promise<[Output<any>]> {
+	throw "Method not implemented";
 }
---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export type Input<T> = {
+	name: string;
+	value: T;
+}
+
+export type Output<T> = {
+	name: string;
+	value: T;
+}
